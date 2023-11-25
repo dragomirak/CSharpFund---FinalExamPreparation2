86f7229ae7fa49508544d6ab525d3cab2cf65b4a
Dragomira <132193889+dragomirak@users.noreply.github.com>
2023-11-25 18:22:54 +0200

    Add project files.

---
+++ P01.ActivationKeys/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Diagnostics.Tracing;
+
+namespace P01.ActivationKeys
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string activationKey = Console.ReadLine();
+
+            string input;
+            while ((input = Console.ReadLine()) != "Generate")
+            {
+                string[] instructions = input.Split(">>>");
+                string command = instructions[0];
+
+                if (command == "Contains")
+                {
+                    string substring = instructions[1];
+                    if (activationKey.Contains(substring))
+                    {
+                        Console.WriteLine($"{activationKey} contains {substring}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Substring not found!");
+                    }
+                }
+                else if (command == "Flip")
+                {
+                    string type = instructions[1];
+                    int startIndex = int.Parse(instructions[2]);
+                    int endIndex = int.Parse(instructions[3]);
+                    int length = endIndex - startIndex;
+                    string stringToModify = activationKey.Substring(startIndex, length);
+
+                    if (type == "Upper")
+                    {
+                        string stringToUpper = stringToModify.ToUpper();
+                        activationKey = activationKey.Replace(stringToModify, stringToUpper);
+                    }
+                    else if (type == "Lower")
+                    {
+                        string stringToLower = stringToModify.ToLower();
+                        activationKey = activationKey.Replace(stringToModify, stringToLower);
+                    }
+                    Console.WriteLine(activationKey);
+                }
+                else if (command == "Slice")
+                {
+                    int startIndex = int.Parse(instructions[1]);
+                    int endIndex = int.Parse(instructions[2]);
+                    int length = endIndex - startIndex;
+                    activationKey = activationKey.Remove(startIndex, length);
+                    Console.WriteLine(activationKey);
+                }
+            }
+
+            Console.WriteLine($"Your activation key is: {activationKey}");
+        }
+    }
+}
---
+++ P02.EmojiDetector/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Text.RegularExpressions;
+
+namespace P02.EmojiDetector
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string coolThreshholdPattern = @"\d";
+            string emojiPattern = @"(:{2}|\*{2})(?<emoji>[A-Z][a-z]{2,})\1";
+            string emojiCoolnessPattern = @"\w";
+
+            List<string> allEmoji = new List<string>();
+            List<string> validEmoji = new List<string>();
+
+            string input = Console.ReadLine();
+            long threshhold = 1;
+
+            foreach (Match match in Regex.Matches(input, coolThreshholdPattern))
+            {
+                threshhold *= long.Parse(match.Value);
+            }
+
+            foreach (Match match in Regex.Matches(input, emojiPattern))
+            {
+                string emoji = match.Value;
+                allEmoji.Add(emoji);
+            }
+
+            foreach (string emoji in allEmoji)
+            {
+                int emojiCoolness = 0;
+                foreach (Match match in Regex.Matches(emoji, emojiCoolnessPattern))
+                {
+                    emojiCoolness += char.Parse(match.Value);
+                }
+
+                if (emojiCoolness >= threshhold)
+                {
+                    validEmoji.Add(emoji);
+                }
+            }
+
+            Console.WriteLine($"Cool threshold: {threshhold}");
+            Console.WriteLine($"{allEmoji.Count} emojis found in the text. The cool ones are:");
+            foreach (string emoji in validEmoji)
+            {
+                Console.WriteLine(emoji);
+            }
+        }
+    }
+}
