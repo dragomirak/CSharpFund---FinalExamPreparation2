305635282037c1c74c09ebc96382e64f2063206c
Dragomira <132193889+dragomirak@users.noreply.github.com>
2023-11-27 18:43:21 +0200

    Adding P03.Heroes

---
+++ P03.HeroesOfCodeAndLogicVII/Hero.cs
@@ -0,0 +1,77 @@
+﻿public class Hero
+{
+    public Hero(string heroName, int hitPoints, int manaPoints)
+    {
+        HeroName = heroName;
+        HitPoints = hitPoints;
+        ManaPoints = manaPoints;
+    }
+
+    public string HeroName { get; set; }
+    public int HitPoints { get; set; }
+    public int ManaPoints { get; set; }
+
+    public void CastSpell(string heroName, int mpNeeded, string spellName)
+    {
+        if( ManaPoints >= mpNeeded)
+        {
+            ManaPoints -= mpNeeded;
+            Console.WriteLine($"{heroName} has successfully cast {spellName} and now has {ManaPoints} MP!\"");
+        }
+        else
+        {
+            Console.WriteLine($"{heroName} does not have enough MP to cast {spellName}!");
+        }
+    }
+
+    public void TakeDamage(string heroName, int damage, string attacker)
+    {
+        HitPoints -= damage;
+        if (HitPoints <= 0)
+        {
+            Console.WriteLine($"{heroName} has been killed by {attacker}!");
+        }
+        else
+        {
+            Console.WriteLine($"{heroName} was hit for {damage} HP by {attacker} and now has {HitPoints} HP left!");
+        }
+    }
+
+    public void Recharge(string heroName, int mpAmount)
+    {
+        int initialManaPoints = ManaPoints;
+        ManaPoints += mpAmount;
+        if (ManaPoints > 200) 
+        { 
+            ManaPoints = 200;
+            Console.WriteLine($"{heroName} recharged for {ManaPoints - initialManaPoints} MP!");
+        }
+        else
+        {
+            Console.WriteLine($"{heroName} recharged for {mpAmount} MP!");
+        }
+
+    }
+
+    public void Heal(string heroName, int hpAmount)
+    {
+        int initialHitPoints = HitPoints;
+        HitPoints += hpAmount;
+        if (HitPoints > 100)
+        {
+            HitPoints = 100;
+            Console.WriteLine($"{heroName} healed for {HitPoints - initialHitPoints} HP!");
+        }
+        else
+        {
+            Console.WriteLine($"{heroName} healed for {hpAmount} HP!");
+        }
+
+    }
+
+    public override string ToString()
+    {
+        string result = $"{HeroName}\r\n  HP: {HitPoints}\r\n  MP: {ManaPoints}";
+        return result;
+    }
+}
---
+++ P03.HeroesOfCodeAndLogicVII/Program.cs
@@ -0,0 +1,62 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace P03.HeroesOfCodeAndLogicVII
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int heroesCount = int.Parse(Console.ReadLine());
+            List<Hero> heroes = new List<Hero>();
+
+            for (int i = 0; i < heroesCount; i++)
+            {
+                string[] heroData = Console.ReadLine().Split(" ").ToArray();
+                string heroName = heroData[0];
+                int hitPoints = int.Parse(heroData[1]);
+                int manaPoints = int.Parse(heroData[2]);
+
+                Hero hero = new Hero(heroName, hitPoints, manaPoints);
+                heroes.Add(hero);
+            }
+
+            string input;
+            while ((input = Console.ReadLine()) != "End")
+            {
+                string[] instructions = input.Split(" - ");
+                string action = instructions[0];
+                string heroName = instructions[1];
+                Hero foundHero = heroes.First(h => h.HeroName == heroName);
+
+                if (action == "CastSpell")
+                {
+                    int mpNeeded = int.Parse(instructions[2]);
+                    string spellName = instructions[3];
+                    foundHero.CastSpell(heroName, mpNeeded, spellName);
+                }
+                else if (action == "TakeDamage")
+                {
+                    int damage = int.Parse(instructions[2]);
+                    string attacker = instructions[3];
+                    foundHero.TakeDamage(heroName, damage, attacker);
+                }
+                else if (action == "Recharge")
+                {
+                    int mpAmount = int.Parse(instructions[2]);
+                    foundHero.Recharge(heroName, mpAmount);
+                }
+                else if (action == "Heal")
+                {
+                    int hpAmount = int.Parse(instructions[2]);
+                    foundHero.Heal(heroName, hpAmount);
+                }
+            }
+
+            heroes.RemoveAll(h => h.HitPoints <= 0);
+            foreach (Hero hero in heroes)
+            {
+                Console.WriteLine(hero.ToString());
+            }
+        }
+    }
+}
